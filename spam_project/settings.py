92a109778a3f9015b94859cb6820c241b1910dc3
grvPratik <pratikgauravv@gmail.com>
2025-02-12 22:05:21 +0000

    ui changes

--- spam_project/settings.py
+++ spam_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oo8pmh=ul^yup-c4qz77^$$7x8u*(-sb#q8aj&ya%wtk%+57+=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.86.111.64']
 
 
 # Application definition
